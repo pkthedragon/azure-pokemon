fed62a1fe8706506f71a1deef880907be6f03f19
pkthedragon <47231243+pkthedragon@users.noreply.github.com>
2022-05-08 08:06:07 -0500

    big move to better base

---
+++ Scripts/PBStatuses.rb
@@ -0,0 +1,16 @@
+#70925035
+begin
+  class PBStatuses
+    SLEEP     = 1
+    POISON    = 2
+    BURN      = 3
+    PARALYSIS = 4
+    FROZEN    = 5
+  end
+
+rescue Exception
+  if $!.is_a?(SystemExit) || "#{$!.class}"=="Reset"
+    raise $!
+  else
+  end
+end
